8e81d78a8b32d47674ac31b2dcd15cce0b02bdef
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-03-21 14:41:15 -0300

    :sparkles: add paginate to category receivements

--- backend/src/resolvers/category-receivement/find-all-category-receivements.resolver.ts
+++ backend/src/resolvers/category-receivement/find-all-category-receivements.resolver.ts
@@ -4,13 +4,14 @@ import { CategoryReceivement } from '@/database/entities/category-receivement.en
 import { FindAllCategoryReceivementsService as IFindAllCategoryReceivementsService } from '@/interfaces/category-receivement/find-all-category-receivements.interface';
 import { FindAllCategoryReceivementsService } from '@/services/category-receivement/find-all-category-receivements.service';
 import { Account } from '@/database/entities/account.entity';
+import CategoryReceivementPaginate from './paginate/category-receivements-input.paginate';
 
-@Resolver(() => CategoryReceivement)
+@Resolver(() => CategoryReceivementPaginate)
 export class FindAllCategoryReceivementsResolver {
   constructor(
     @Inject(FindAllCategoryReceivementsService) private findAllCategoryReceivements: IFindAllCategoryReceivementsService,
   ) { }
-  @Query(() => [CategoryReceivement])
+  @Query(() => CategoryReceivementPaginate)
   async categoryReceivements(@Args('account', { type: () => ID }) account: Account['id'],): Promise<CategoryReceivement[]> {
     return this.findAllCategoryReceivements.find(account);
   }
