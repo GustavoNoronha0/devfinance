cc1feb56e392223b721410f99260a2a452fe133c
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-03-21 14:10:27 -0300

    :sparkles: add resolvers to graph

---
+++ backend/src/resolvers/graph/find-account-graph.resolver.ts
@@ -0,0 +1,18 @@
+import { Inject } from '@nestjs/common';
+import { Args, ID, Query, Resolver } from '@nestjs/graphql';
+import { AccountGraph } from '@/interfaces/graph/account-graph.typing';
+import { FindAccountGraphService as IFindAccountGraphService } from '@/interfaces/graph/find-account-graph.interface';
+import { FindAccountGraphService } from '@/services/graph/find-account-graph.service';
+
+@Resolver(() => AccountGraph)
+export class FindAccountGraphResolver {
+  constructor(
+    @Inject(FindAccountGraphService) private findAccountGraphService: IFindAccountGraphService,
+  ) { }
+  @Query(() => AccountGraph, { nullable: true })
+  async graphAccount(
+    @Args('account', { type: () => ID }) account: string,
+  ): Promise<AccountGraph> {
+    return this.findAccountGraphService.findGraph(account);
+  }
+}
