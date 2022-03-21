0cc9452add56b985155c7d5d076113f3851e81e5
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-03-21 14:39:15 -0300

    :sparkles: add paginate to category debits resolver

--- backend/src/resolvers/category-debit/find-all-category-debits.resolver.ts
+++ backend/src/resolvers/category-debit/find-all-category-debits.resolver.ts
@@ -4,13 +4,14 @@ import { CategoryDebit } from '@/database/entities/category-debit.entity';
 import { FindAllCategoryDebitsService as IFindAllCategoryDebitsService } from '@/interfaces/category-debit/find-all-category-debits.interface';
 import { FindAllCategoryDebitsService } from '@/services/category-debit/find-all-category-debits.service';
 import { Account } from '@/database/entities/account.entity';
+import CategoryDebitPaginate from './paginate/category-debits-input.paginate';
 
-@Resolver(() => CategoryDebit)
+@Resolver(() => CategoryDebitPaginate)
 export class FindAllCategoryDebitsResolver {
   constructor(
     @Inject(FindAllCategoryDebitsService) private findAllCategoryDebits: IFindAllCategoryDebitsService,
   ) { }
-  @Query(() => [CategoryDebit])
+  @Query(() => CategoryDebitPaginate)
   async categoryDebits(@Args('account', { type: () => ID }) account: Account['id'],): Promise<CategoryDebit[]> {
     return this.findAllCategoryDebits.find(account);
   }
