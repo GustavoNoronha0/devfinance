044f16c10f00006ce2efe7e6e2be972b91086819
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-03-23 08:55:31 -0300

    :sparkles: add auth guard to reports

--- backend/src/resolvers/reports/find-account-reports.resolver.ts
+++ backend/src/resolvers/reports/find-account-reports.resolver.ts
@@ -1,9 +1,11 @@
-import { Inject } from '@nestjs/common';
+import { Inject, UseGuards } from '@nestjs/common';
 import { Args, ID, Query, Resolver } from '@nestjs/graphql';
 import { AccountReports } from '@/interfaces/reports/account-reports.typing';
 import { FindAccountReportsService as IFindAccountReportsService } from '@/interfaces/reports/find-account-reports.interface';
 import { FindAccountReportsService } from '@/services/reports/find-account-reports.service';
+import { GqlAuthGuard } from '@/auth/auth.guard';
 
+@UseGuards(GqlAuthGuard)
 @Resolver(() => AccountReports)
 export class FindAccountReportsResolver {
   constructor(
