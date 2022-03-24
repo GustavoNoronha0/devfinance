e471c0333404e5297da7ca099589f3c08a8e7556
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-03-23 22:17:19 -0300

    :recycle: change auth module to folder modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
-import AuthModule from './auth/auth.module';
+import AuthModule from './modules/auth.module';
 import graphqlConfig from './configs/graphql.config';
 import { ReceivementModule } from './modules/receivement.module';
 import { DebitModule } from './modules/debit.module';
--- backend/src/modules/auth.module.ts
+++ backend/src/modules/auth.module.ts
@@ -4,9 +4,9 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import AuthResolver from './auth.resolver';
-import AuthService from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
+import AuthResolver from '../auth/auth.resolver';
+import AuthService from '../auth/auth.service';
+import { JwtStrategy } from '../auth/jwt.strategy';
 
 @Module({
   imports: [
