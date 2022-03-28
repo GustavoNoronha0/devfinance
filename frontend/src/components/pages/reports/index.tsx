afb748832d299b60fab75f6c3213d9ff26c21f45
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-03-28 09:25:27 -0300

    :sparkles: add reports page with cards

--- frontend/src/components/pages/reports/index.tsx
+++ frontend/src/components/pages/reports/index.tsx
@@ -1,10 +1,13 @@
+import Card from '@/components/Card'
 import React from 'react'
 import * as S from './styles'
 
 const Reports = () => {
   return (
     <S.Container>
-      Reports
+      <Card count="1200.00" title="Quantidade a Pagar" onClick={() => console.log("debit")} />
+      <Card count="2300.00" title="Quantidade a Receber" onClick={() => console.log("receivement")} />
+      <Card count="1100.00" title="Quantidade Total" onClick={() => console.log("total")} />
     </S.Container>
   )
 }
--- frontend/src/components/pages/reports/styles.ts
+++ frontend/src/components/pages/reports/styles.ts
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 export const Container = styled.div`
   height: 60vh;
   width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 320px));
+  grid-row-gap: 10px;
 
-  display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
 `
