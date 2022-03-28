19b93ca97546f6b7b793a8a6e40aaca1f1d11874
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-03-27 21:52:58 -0300

    :sparkles: add initial reports page

---
+++ frontend/src/components/pages/reports/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './styles'
+
+const Reports = () => {
+  return (
+    <S.Container>
+      Reports
+    </S.Container>
+  )
+}
+
+export default Reports
