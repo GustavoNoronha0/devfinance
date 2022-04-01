e12e9a430bd41b5f5a46f3f79c5b0846c533e75d
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-04-01 14:04:28 -0300

    :sparkles: change layout of reports

--- frontend/src/components/pages/reports/index.tsx
+++ frontend/src/components/pages/reports/index.tsx
@@ -2,37 +2,60 @@ import { VictoryChart, VictoryPie } from 'victory';
 import Card from '@/components/Card'
 import React from 'react'
 import * as S from './styles'
+import Button from '@/components/Button';
 
 const Reports = () => {
   return (
     <S.Container>
-      <S.Cards>
-        <Card count="1200.00" title="Quantidade a Pagar" onClick={() => console.log("debit")} />
-        <Card count="2300.00" title="Quantidade a Receber" onClick={() => console.log("receivement")} />
-        <Card count="1100.00" title="Quantidade Total" onClick={() => console.log("total")} />
-      </S.Cards>
-      <S.Graph>
-         <VictoryPie
-            padAngle={4}
-            animate={{
-              duration: 2000
-            }}
-            innerRadius={100}
-            colorScale={["tomato", "orange"]}
-            data={[
-                { x: "Pagar", y: 1200 },
-                { x: "Receber", y: 2300 },
-              ]}
-            style={{
-              data: {
-                fillOpacity: 0.9, stroke: "#fff", strokeWidth: 3
-              },
-              labels: {
-                fill: "#fff"
-              }
-            }}
+      <S.Div>
+        <S.Graph>
+          <VictoryPie
+              padAngle={4}
+              animate={{
+                duration: 2000
+              }}
+              innerRadius={100}
+              colorScale={["tomato", "orange"]}
+              data={[
+                  { x: "Pagar", y: 1200 },
+                  { x: "Receber", y: 2300 },
+                ]}
+              style={{
+                data: {
+                  fillOpacity: 0.9, stroke: "#fff", strokeWidth: 3
+                },
+                labels: {
+                  fill: "#fff"
+                }
+              }}
           />
-      </S.Graph>
+          <S.Reports>
+            <S.ReportsTitle>
+              Quantidade a Pagar: <S.ReportsValueDebit>10</S.ReportsValueDebit>
+            </S.ReportsTitle>
+            <S.ReportsTitle>
+              Quantidade a Receber: <S.ReportsValueReceivement>10</S.ReportsValueReceivement>
+            </S.ReportsTitle>
+            <S.ReportsTitle>
+              Quantidade total: <S.ReportsValue>10</S.ReportsValue>
+            </S.ReportsTitle>
+          </S.Reports>
+          <S.ButtonAdd>
+            <Button
+              typeStyle="add" 
+              children="Baixar Relatorios" 
+              onClick={() => {
+                console.log('reports')
+              }}
+            />
+          </S.ButtonAdd> 
+        </S.Graph>
+        <S.Cards>
+          <Card count="1200.00" title="Saldo a Pagar" onClick={() => console.log("debit")} />
+          <Card count="2300.00" title="Saldo a Receber" onClick={() => console.log("receivement")} />
+          <Card count="1100.00" title="Saldo Total" onClick={() => console.log("total")} />
+        </S.Cards>
+      </S.Div>
     </S.Container>
   )
 }
--- frontend/src/components/pages/reports/styles.ts
+++ frontend/src/components/pages/reports/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
   width: 100%;
@@ -8,6 +8,67 @@ export const Container = styled.div`
 
   align-items: center;
   justify-content: center;
+`
+
+export const Div = styled.div`
+  width: 80%;
+`
+
+export const Graph = styled.div`
+  margin: 10px 60px; 
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 320px));
+  grid-row-gap: 10px;
+
+  align-items: center;
+  justify-content: space-arround;
+`
+
+export const Reports = styled.div`
+  margin-left: 50px;
+`
+
+export const ReportsTitle = styled.h4`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+  `}
+  display: flex;
+  flex-direction: row;
+`
+
+export const ReportsValue = styled.h4`
+  ${({ theme }) => css`
+    color: ${theme.colors.baseColorYellow};
+    margin-left: 5px;
+  `}
+`
+
+export const ReportsValueDebit = styled.h4`
+  ${({ theme }) => css`
+    color: ${theme.colors.tomato};
+    margin-left: 5px;
+  `}
+`
+
+export const ReportsValueReceivement = styled.h4`
+  ${({ theme }) => css`
+    color: ${theme.colors.baseColorGreen};
+    margin-left: 5px;
+  `}
+`
+
+export const ButtonAdd = styled.div`
+  margin-left: 20px;
+
+
+  @media (max-width: 800px) {
+    margin-left: 40px;
+  }
+
+  @media (max-width: 600px) {
+    margin-left: 40px;
+  }
 `
 
 export const Cards = styled.div`
@@ -21,6 +82,3 @@ export const Cards = styled.div`
   margin-top: 40px;
 `
 
-export const Graph = styled.div`
-  margin-top: 10px; 
-`
